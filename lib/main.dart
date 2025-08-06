a9b38ab8e41da90b4b4db71f17a09562755a4496
pahri-programming <pahriabdurohim03@gmail.com>
2025-08-06 17:04:08 +0700

    belajar auth login dan register

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-
-import 'package:apiflutter/pages/photo/list_photo_screen.dart';
-import 'package:apiflutter/pages/posts/list_post_screen.dart';
+import 'package:apiflutter/pages/auth/login_screen.dart';
+import 'package:apiflutter/pages/home_screen.dart';
+import 'package:apiflutter/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +13,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fatch Data'),
-        ),
-        body: ListPhotoScreen(),
-      ),
+      title: 'Auth Example',
+      home: AuthCheck(),
     );
   }
 }
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({super.key});
+
+  @override
+  State<AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  final AuthService _authService = AuthService();
+  late Future<bool> _isLoggedIn;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoggedIn = _authService.isLoggedIn();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _isLoggedIn,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData && snapshot.data == true) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+}
+
+
+// import 'package:apiflutter/pages/photo/list_photo_screen.dart';
+// import 'package:apiflutter/pages/posts/list_post_screen.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Fatch Data'),
+//         ),
+//         body: ListPhotoScreen(),
+//       ),
+//     );
+//   }
+// }
